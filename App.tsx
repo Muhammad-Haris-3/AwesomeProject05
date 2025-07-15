28221741b73fced81f901316e149e24fb9713b5e
Muhammad-Haris-3 <hariskhokhar975@gmail.com>
2025-07-15 16:41:21 +0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,27 +1,119 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { PropsWithChildren, useState } from 'react';
+import {
+  Image,
+  ImageSourcePropType,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import One from './assets/images/1_on_dice.png';
+import Two from './assets/images/2_on_dice.png';
+import Three from './assets/images/3_on_dice.png';
+import Four from './assets/images/4_on_dice.png';
+import Five from './assets/images/5_on_dice.png';
+import Six from './assets/images/6_on_dice.png';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+type DiceProps = PropsWithChildren<{
+  imageURL: ImageSourcePropType;
+}>;
 
+const Dice = ({ imageURL }: DiceProps) => {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View>
+      <Image style={styles.image} source={imageURL} />
     </View>
   );
-}
+};
+
+const App = () => {
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(One);
+
+  const diceRoller = () => {
+    let randomNuber = Math.floor(Math.random() * 6) + 1;
+
+    switch (randomNuber) {
+      case 1:
+        setDiceImage(One);
+        break;
+
+      case 2:
+        setDiceImage(Two);
+        break;
+
+      case 3:
+        setDiceImage(Three);
+        break;
+
+      case 4:
+        setDiceImage(Four);
+        break;
+
+      case 5:
+        setDiceImage(Five);
+        break;
+
+      case 6:
+        setDiceImage(Six);
+        break;
+
+      default:
+        break;
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.mainHeading}>Alhamdulillah</Text>
+      <View style={styles.imageContainer}>
+        <Dice imageURL={diceImage} />
+      </View>
+      <Pressable onPress={diceRoller} style={styles.buttonTextContainer}>
+        <View>
+          <Text style={styles.buttonText}>Roll Dice</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  mainHeading: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 700,
+    fontSize: 36,
+    marginTop: 20,
+  },
+  image: {
+    height: 100,
+    width: 100,
+    margin: 10,
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 700,
+    fontSize: 20,
+    padding: 2,
+    backgroundColor: 'green',
+    height: 60,
+    width: 60,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: 'black',
+    margin: 5,
+  },
+  buttonTextContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
